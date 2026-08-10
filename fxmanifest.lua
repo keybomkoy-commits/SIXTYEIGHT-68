585cd1207c08bb1ad5c3d3a75ea6fe6166062410
keybomkoy-commits <keybomkoy@gmail.com>
2026-08-10 20:46:29 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author '9841 ELITESTYLE'
+
+description '9841 ELITESTYLE Website'
+
+ui_page 'index.html'
+
+files {
+    'index.html',
+    'login.html',
+    'admin.html',
+    'gang.html',
+    'family.html'
+}
+
+client_script 'client.lua'
